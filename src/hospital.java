00527ffd31072bc649b00a48aa0593be1b0334cf
vishal8202 <vishalvijayan222@gmail.com>
2022-11-09 22:04:47 +0530

    add

--- src/hospital.java
+++ src/hospital.java
@@ -10,6 +10,11 @@ public class hospital {
         String patientName, patientAddress, patientSymptoms, patientsDoctor, patientPhone;
         int patientId, patientPincode;
 
+
+
+
+
+
         while(true) {
             System.out.println("Select an option from below");
             System.out.println("1.Add Patient ");
@@ -24,7 +29,44 @@ public class hospital {
             switch (choice){
                 case 1:
                     System.out.println("Add a Patient");
+                    System.out.println("Enter Patient ID: ");
+                    patientId = input.nextInt();
+                    System.out.println("Enter Patient Name: ");
+                    patientName = input.next();
+                    System.out.println("Enter patient address: ");
+                    patientAddress = input.next();
+                    System.out.println("Enter pincode: ");
+                    patientPincode = input.nextInt();
+                    System.out.println("Enter patient phone: ");
+                    patientPhone = input.next();
+                    System.out.println("Enter patients symptoms: ");
+                    patientSymptoms = input.next();
+                    System.out.println("Enter doctors name: ");
+                    patientsDoctor = input.next();
+
+                    try {
+                        Class.forName("com.mysql.jdbc.Driver");
+                        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hospitaldb","root","");
+                        String sql = "INSERT INTO `patients`(`patientid`, `name`, `address`, `pincode`, `phone`, `symptoms`, `docname`)VALUES (?,?,?,?,?,?,?)";
+                        PreparedStatement stmt = con.prepareStatement(sql);
+                        stmt.setInt(1,patientId);
+                        stmt.setString(2,patientName);
+                        stmt.setString(3,patientAddress);
+                        stmt.setInt(4,patientPincode);
+                        stmt.setString(5,patientPhone);
+                        stmt.setString(6,patientSymptoms);
+                        stmt.setString(7,patientsDoctor);
+                        stmt.executeUpdate();
+                    }
+                    catch (Exception e ){
+                        System.out.println(e);
+                    }
+
+
+
+
                     break;
+
                 case 2:
                     System.out.println("View all Patients");
 
@@ -32,13 +74,16 @@ public class hospital {
                 case 3:
                     System.out.println("Search Patient");
 
+
                     break;
                 case 4:
                     System.out.println("Update Patient Details");
 
+
                     break;
                 case 5:
                     System.out.println("Delete Patient");
+
 
                     break;
                 case 6:
@@ -47,6 +92,7 @@ public class hospital {
                     break;
                 default:
                     System.out.println("Enter correct choice");
+
 
             }
 
